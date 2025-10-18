9f2701674748080624f2b2b52148f22617938c0c
Pasala Bala Arun <108626342+arunpasala@users.noreply.github.com>
2025-10-18 15:56:07 -0400

    numbers add sub div mul

---
+++ num.py
@@ -0,0 +1,18 @@
+addition = 5 + 3
+print("add: ",addition)
+
+subtraction = 13-5
+print("sub: ",subtraction)
+
+multiplication = 2*4
+print("mul: ",multiplication)
+
+division = 16//2
+print("div: ",division)
+
+division = 16/2
+print("div: ",division)
+
+
+add = 0.2 +0.1
+print(add)
