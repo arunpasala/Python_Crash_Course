07ff830fd478040cba26b4d8cf4a13c42469ad21
Pasala Bala Arun <108626342+arunpasala@users.noreply.github.com>
2025-10-24 17:22:20 -0400

    list assignment

---
+++ sort()_assignment.py
@@ -0,0 +1,18 @@
+places= ["canada","egypt","mexico","italy","arab"]
+print(f"original order: {places}")
+print(f"sorted  order: {sorted(places)}")
+print(f"Original order agian: {places}")
+
+print(sorted(places, reverse=True))
+print(f"original order: {places}")
+
+places.reverse()
+print(places)
+
+places.reverse()
+print(places)
+
+places.sort()
+print(places)
+
+
