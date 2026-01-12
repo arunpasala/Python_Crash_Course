e5d6dbf02c9cc2aa1e01cc5dd20c3f8c8f277dc4
Pasala Bala Arun <108626342+arunpasala@users.noreply.github.com>
2026-01-12 12:51:18 -0500

    4.1 try it yourself in list using for loop

---
+++ forLoop.py
@@ -0,0 +1,12 @@
+magicians=['alice','david','carolina']
+print(f"These are the top three magicians \n{magicians}")
+
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick")
+    
+print("Thank you everyone that was a great show..!")
+    
+    
+
+
+
---
+++ pizzas.py
@@ -0,0 +1,6 @@
+pizzas = ['pepporoni', 'margherita', 'hawaiian', 'bbq chicken', 'veggie']
+msg="I like "
+for pizza in pizzas:
+    print(f"{msg} {pizza} pizza.")
+    
+print("I really love pizza!")   
