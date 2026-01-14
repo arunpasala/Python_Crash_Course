9405cce23917337739d25f3e7c9547de6bbc5353
Pasala Bala Arun <108626342+arunpasala@users.noreply.github.com>
2026-01-14 11:46:17 -0500

    working with dictionaries assigning and modifying concepts

--- Dictionaries/alien.py
+++ Dictionaries/alien.py
@@ -8,4 +8,30 @@ print(alien_0['points'])
 #Accessing valuse in a dictionary
 
 new_points = alien_0['points']
-print(f'You just earned{new_points} points!')
+print(f'You just earned {new_points} points!')
+
+#Adding a new key value pair to a dictionary
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
+alien_0['name']='arun'
+
+print(alien_0)
+
+print("all the key values pairs are removed now but only age is added: ")
+alien_0 ={'age':25}
+print(alien_0)
+
+
+#modifying values in a dictionary
+print("The alien is green, nnow the age is replaced with color")
+alien_0 = {'color':'green'}
+print(f"The alien is {alien_0['color']}.")
+
+alien_0['color'] = 'yellow'
+print(f"The alien is now {alien_0['color']}.")
+
+print(alien_0)
+
