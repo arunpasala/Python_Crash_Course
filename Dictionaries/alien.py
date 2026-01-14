ea28db0abb11860ecd272662dd0763af26e4f98f
Pasala Bala Arun <108626342+arunpasala@users.noreply.github.com>
2026-01-14 11:20:18 -0500

    working with dictionaries

---
+++ Dictionaries/alien.py
@@ -0,0 +1,11 @@
+alien_0 = {'color':'green', 'points':5}
+print(alien_0['color'])
+print(alien_0['points'])
+
+#A dictionary is a collection of key value pairs
+#You can add new key value pairs to a dictionary
+
+#Accessing valuse in a dictionary
+
+new_points = alien_0['points']
+print(f'You just earned{new_points} points!')
