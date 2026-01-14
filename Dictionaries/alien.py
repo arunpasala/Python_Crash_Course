e00976b3b95a8262fa9d44344d8ec43984d01481
Pasala Bala Arun <108626342+arunpasala@users.noreply.github.com>
2026-01-14 12:01:30 -0500

    working with removing key value pairs

--- Dictionaries/alien.py
+++ Dictionaries/alien.py
@@ -35,3 +35,27 @@ print(f"The alien is now {alien_0['color']}.")
 
 print(alien_0)
 
+alien_0 = {'x_position':0, 'y_position':25, 'speed':'medium'}
+print(f'Original position: {alien_0["x_position"]}')
+#Move the alien to the right
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+
+#The new position is the old position plus the increment
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print(f'New position: {alien_0["x_position"]}')
+
+alien_0['speed'] = 'fast'
+print(f'Original position: {alien_0["x_position"] + x_increment}')
+print(f'The alien is now {alien_0["speed"]}.')
+
+print("\n")
+#del a key value pair
+print(f"before removing key value pair :\n {alien_0}")
+del alien_0['speed']
+print(f"after removing key value pair :\n {alien_0}")
+
