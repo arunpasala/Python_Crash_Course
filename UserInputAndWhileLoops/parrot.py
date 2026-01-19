7858887a8ebe568c04534994c1c144bec7f42ad4
Pasala Bala Arun <108626342+arunpasala@users.noreply.github.com>
2026-01-19 13:03:23 -0500

    Working with User Inputs and While Loops

---
+++ UserInputAndWhileLoops/parrot.py
@@ -0,0 +1,7 @@
+promt = "\n Tell me something and I will repeat it back to you:"
+promt += "\n Enter 'quit' to end the program. "
+message = ""
+while message != 'quit':
+    message = input(promt)
+    print(message)
+        
