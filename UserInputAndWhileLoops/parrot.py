07b95e17ad07f491a2926c48c24f1ea9d521e9bc
Pasala Bala Arun <108626342+arunpasala@users.noreply.github.com>
2026-01-20 12:53:45 -0500

    finished dictionaries concept using for, while and if along with lists

--- UserInputAndWhileLoops/parrot.py
+++ UserInputAndWhileLoops/parrot.py
@@ -1,7 +1,10 @@
 promt = "\n Tell me something and I will repeat it back to you:"
 promt += "\n Enter 'quit' to end the program. "
-message = ""
-while message != 'quit':
+active = True
+while active:
     message = input(promt)
-    print(message)
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
         
