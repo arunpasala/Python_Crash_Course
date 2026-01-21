c13c7b2730ed5dbb008d573a599af53e8dcefdb8
Pasala Bala Arun <108626342+arunpasala@users.noreply.github.com>
2026-01-20 23:57:58 -0500

    positional arguments concept

--- Functions/pets.py
+++ Functions/pets.py
@@ -4,3 +4,6 @@ def describe_pet(animal_type, pet_name):
     print(f"\nI have a {animal_type}.")
     print(f"My {animal_type}'s name is {pet_name.title()}.")
 describe_pet('hamster', 'harry')
+describe_pet('dog', 'willie')
+
+#values matched this way are called positional arguments because their position matters.
