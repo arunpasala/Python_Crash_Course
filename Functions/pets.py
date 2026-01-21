e504c8bc7444e5576b1e39dde2caef71b1faab09
Pasala Bala Arun <108626342+arunpasala@users.noreply.github.com>
2026-01-20 23:55:38 -0500

    Positional Arguments

---
+++ Functions/pets.py
@@ -0,0 +1,6 @@
+#positional arguments: Python must match the order of the parameters in the function definition.
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+describe_pet('hamster', 'harry')
